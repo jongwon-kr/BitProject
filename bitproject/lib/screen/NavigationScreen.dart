ba1d6eb24aa4cea26f714cf30951b4684c52b0c0
jongwon-kr <growjong8802@gmail.com>
2023-11-14 22:48:19 +0900

    home, navigation

---
+++ bitproject/lib/screen/NavigationScreen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:medicalapp/screens/qna_screen.dart';
+import 'package:medicalapp/screens/dictionary_screen.dart';
+import 'package:medicalapp/screens/home_screen.dart';
+
+import '../screens/health_check_screen.dart';
+import '../screens/my_info_screen.dart';
+
+List<String> titles = <String>[
+  '질환백과',
+  '문진',
+  'Home',
+  'Q&A',
+  '내정보',
+];
+
+List<Widget> screens = <Widget>[
+  const DicionaryScreen(),
+  const healthCheckScreen(),
+  const HomeScreen(),
+  const ChatPage(),
+  const MyInfoScreen(),
+];
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
+
+  get scaffoldKey => null;
+  @override
+  State<NavigationScreen> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<NavigationScreen> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  int currentPageIndex = 2; // 2번이 Home
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+
+    return Scaffold(
+      key: scaffoldKey,
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.grey[400],
+        elevation: 1.0,
+        selectedIndex: currentPageIndex,
+        destinations: <Widget>[
+          NavigationDestination(
+            selectedIcon: const Icon(Icons.manage_search),
+            icon: const Icon(Icons.manage_search_outlined),
+            label: titles[0],
+          ),
+          NavigationDestination(
+            selectedIcon: const Icon(Icons.article),
+            icon: const Icon(Icons.article_outlined),
+            label: titles[1],
+          ),
+          NavigationDestination(
+            selectedIcon: const Icon(Icons.home),
+            icon: const Icon(Icons.home_outlined),
+            label: titles[2],
+          ),
+          NavigationDestination(
+            selectedIcon: const Icon(Icons.question_answer),
+            icon: const Icon(Icons.question_answer_outlined),
+            label: titles[3],
+          ),
+          NavigationDestination(
+            selectedIcon: const Icon(Icons.person),
+            icon: const Icon(Icons.person_outlined),
+            label: titles[4],
+          ),
+        ],
+      ),
+      body: <Widget>[
+        screens[0],
+        screens[1],
+        screens[2],
+        screens[3],
+        screens[4],
+      ][currentPageIndex],
+    );
+  }
+}
