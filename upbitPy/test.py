abf566fa6c27805515d5a974160d0d3c6ed6d05e
jongwon-kr <growjong8802@gmail.com>
2023-12-04 18:13:00 +0900

    Update test.py

--- upbitPy/test.py
+++ upbitPy/test.py
@@ -1,1 +1,7 @@
-11
+import requests
+
+
+def get_ticker_price(market):
+    url = f"https://api.upbit.com/vi/ticker?markets={market}"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
