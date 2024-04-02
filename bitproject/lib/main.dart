fee6707fc349617673ebcce2571e03bef755f63c
jongwon-kr <growjong8802@gmail.com>
2024-04-02 23:43:02 +0900

    asd

--- bitproject/lib/main.dart
+++ bitproject/lib/main.dart
@@ -1,22 +1,105 @@
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+
+// import 'package:medicalapp/screen/navigation_screen.dart';
+
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MainApp());
+// }
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: NavigationScreen(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import 'package:medicalapp/screen/navigation_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:medicalapp/CoinList.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const title = 'WebSocket Demo';
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: NavigationScreen(),
+      title: title,
+      home: MyHomePage(
+        title: title,
+      ),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCoinPrice();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Form(
+                  child: TextFormField(
+                    controller: _controller,
+                    decoration:
+                        const InputDecoration(labelText: 'Send a message'),
+                  ),
+                ),
+                const SizedBox(height: 24),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void fetchCoinPrice() async {
+    final response = await http
+        .get(Uri.parse("https://api.bithumb.com/public/ticker/ALL_KRW"));
+
+    if (response.statusCode == 200) {
+      CoinList coinList = coinListFromJson(response.body);
+      print(response.body);
+    }
   }
 }
